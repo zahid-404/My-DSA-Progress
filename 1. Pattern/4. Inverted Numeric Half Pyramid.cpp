a83582780fc5c695c37fab94398d1f8f968413da
Zahid <zahidmohammad495@gmai.com>
2023-09-05 10:06:36 +0530

    Complete problems of pattern class

---
+++ 1. Pattern/4. Inverted Numeric Half Pyramid.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout << "enter input";
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+
+    for (int j = 0; j < n - i; j++) {
+      cout << j + 1 << " ";
+    }
+    cout << endl;
+  }
+}
